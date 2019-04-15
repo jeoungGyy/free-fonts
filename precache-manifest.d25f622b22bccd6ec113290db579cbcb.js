a3655796dd5f8c755682fa0f4aefa02ab0821d7a
jeoungGyy <limjungkoro@gmail.com>
2019-04-15 16:02:22 +0900

    Updates

---
+++ precache-manifest.d25f622b22bccd6ec113290db579cbcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "576e08e28469e2c1d916",
+    "url": "/free-fonts/static/js/runtime~main.e7721dbf.js"
+  },
+  {
+    "revision": "799cbe26d143e10aeaa9",
+    "url": "/free-fonts/static/js/main.682a5a7e.chunk.js"
+  },
+  {
+    "revision": "d7056ff15114d1353ca3",
+    "url": "/free-fonts/static/js/2.a0060353.chunk.js"
+  },
+  {
+    "revision": "799cbe26d143e10aeaa9",
+    "url": "/free-fonts/static/css/main.fb12f609.chunk.css"
+  },
+  {
+    "revision": "d7056ff15114d1353ca3",
+    "url": "/free-fonts/static/css/2.c96109ab.chunk.css"
+  },
+  {
+    "revision": "1e0550fc6e3179ca994d7934f36f5b21",
+    "url": "/free-fonts/index.html"
+  }
+];
